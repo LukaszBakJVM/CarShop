9b5be63a31fdc637107762349e8d39b5be436239
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-12-04 14:55:59 +0100

    Rest Controller

---
+++ src/main/java/com/example/carshop/App/Car/Category/CategoryController.java
@@ -0,0 +1,32 @@
+package com.example.carshop.App.Car.Category;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    ResponseEntity<List<String>> allCategory() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping
+    ResponseEntity<Category> save(@RequestBody Category category) {
+        Category saveCategory = service.newCategory(category);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .buildAndExpand(saveCategory.getName()).toUri();
+        return ResponseEntity.created(uri).body(saveCategory);
+    }
+
+}
